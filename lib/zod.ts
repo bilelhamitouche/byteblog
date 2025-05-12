1f2a18a0276c06dd7a8fe142919d683cdfdcfeb5
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-13 00:44:32 +0100

    add schema files for zod

---
+++ lib/zod.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().trim().email({ message: "Must be a valid email address" }),
+  password: z
+    .string()
+    .trim()
+    .min(8, { message: "Must be at least 8 characters long" })
+    .max(40, { message: "Must be at most 40 characters long" }),
+});
+
+export const registerSchema = z.object({
+  name: z.string().trim().min(1, { message: "Name is required" }),
+  email: z.string().trim().email({ message: "Must be a valid email address" }),
+  password: z
+    .string()
+    .trim()
+    .min(8, { message: "Must be at least 8 characters long" })
+    .max(40, { message: "Must be at most 40 characters long" }),
+});
