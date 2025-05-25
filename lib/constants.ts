45fddd8a923adc08a47cf9202f862fff9a18730d
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-25 13:59:27 +0100

    add avatarColor generation function and avatarColors array

---
+++ lib/constants.ts
@@ -0,0 +1,12 @@
+export const avatarColors = [
+  "bg-red-700 text-white",
+  "bg-green-700 text-white",
+  "bg-yellow-700 text-white",
+  "bg-blue-700 text-white",
+  "bg-violet-700 text-white",
+  "bg-orange-700 text-white",
+  "bg-cyan-700 text-white",
+  "bg-stone-700 text-white",
+  "bg-pink-700 text-white",
+  "bg-gray-200 text-black",
+];
