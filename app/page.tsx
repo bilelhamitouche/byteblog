941e5ea8235eaabd7345322e39059ce72253929f
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-07-23 07:09:03 +0100

    add landing page to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,23 @@
+import { getUserInfo } from "@/actions/auth";
+import LandingPage from "@/components/landing-page";
 import { Button } from "@/components/ui/button";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-  if (!session) redirect("/login");
+  const user = await getUserInfo();
+  console.log(user);
+  if (!user) return <LandingPage />;
   return (
-    <div className="flex flex-col gap-2 justify-center items-center h-full">
-      <p>{session?.user.name}</p>
-      <p>{session?.user.username}</p>
-      <form
-        action={async () => {
-          "use server";
-          auth.api.signOut({
-            headers: await headers(),
-          });
-          redirect("/login");
-        }}
-      >
-        <Button variant="destructive">Sign Out</Button>
-      </form>
-    </div>
+    <form
+      action={async () => {
+        "use server";
+        auth.api.signOut({
+          headers: await headers(),
+        });
+      }}
+    >
+      <Button variant="destructive">Sign Out</Button>
+    </form>
   );
 }
