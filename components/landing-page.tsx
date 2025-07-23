bca491b1e1b9fa2e33e2d472c828946e99f95024
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-07-23 07:08:38 +0100

    add landing page

---
+++ components/landing-page.tsx
@@ -0,0 +1,11 @@
+import Hero from "./hero";
+import WhyChooseByteblog from "./why-choose-byteblog";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Hero />
+      <WhyChooseByteblog />
+    </div>
+  );
+}
