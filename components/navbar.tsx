a6e50842c1e122ff8c78b5dfc6ef2931beffc933
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-07-27 02:57:15 +0100

    add avatar dropdown to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,8 +2,13 @@ import { BookOpen } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import MobileMenu from "./mobile-menu";
+import { ModeToggle } from "./mode-toggle";
+import { getUserInfo, isAuthenticated } from "@/actions/auth";
+import AvatarDropdown from "./avatar-dropdown";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const authenticated = await isAuthenticated();
+  const user = await getUserInfo();
   return (
     <header className="container flex fixed z-10 gap-8 justify-between items-center p-4 mx-auto w-full max-w-7xl border-b-1 backdrop-blur-lg bg-background/60">
       <Link href="/" className="flex gap-2 items-center">
@@ -18,15 +23,27 @@ export default function Navbar() {
           Categories
         </Link>
       </nav>
-      <div className="hidden gap-2 items-center md:flex">
-        <Button variant="outline" asChild>
-          <Link href="/login">Login</Link>
-        </Button>
-        <Button asChild>
-          <Link href="/register">Get Started</Link>
-        </Button>
-      </div>
-      <MobileMenu />
+      {authenticated ? (
+        <div className="hidden gap-4 items-center md:flex">
+          <ModeToggle />
+          <AvatarDropdown
+            name={user?.name as string}
+            email={user?.email as string}
+            image={user?.image as string}
+          />
+        </div>
+      ) : (
+        <div className="hidden gap-2 items-center md:flex">
+          <Button variant="outline" asChild>
+            <Link href="/login">Login</Link>
+          </Button>
+          <Button asChild>
+            <Link href="/register">Get Started</Link>
+          </Button>
+          <ModeToggle />
+        </div>
+      )}
+      <MobileMenu authenticated={authenticated} />
     </header>
   );
 }
