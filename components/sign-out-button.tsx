4405de2b46c0df4d22b642a5e57ed495424f6254
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-07-27 02:57:06 +0100

    add signout button

---
+++ components/sign-out-button.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { LogOut } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { DropdownMenuItem } from "./ui/dropdown-menu";
+
+export default function SignOutButton() {
+  const router = useRouter();
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => router.refresh(),
+      },
+    });
+  }
+
+  return (
+    <DropdownMenuItem onClick={signOut}>
+      <LogOut />
+      <span>Logout</span>
+    </DropdownMenuItem>
+  );
+}
