5b78bf2eb47a6077f043d287db2b56be794a1ade
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-09-02 13:12:26 +0100

    add create post and draft queries

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,6 +1,6 @@
 import "server-only";
-import { db, user } from "./drizzle";
-import { eq } from "drizzle-orm";
+import { db, draft, post, user } from "./drizzle";
+import { DrizzleError, eq } from "drizzle-orm";
 import { isAuthenticated } from "@/actions/auth";
 import { redirect } from "next/navigation";
 
@@ -11,4 +11,38 @@ export async function getUserByUsername(username: string) {
     .from(user)
     .where(eq(user.username, username));
   return newUser;
+}
+
+export async function createPost(
+  title: string,
+  image: string | null,
+  content: string,
+  authorId: string,
+  categoryId: string,
+) {
+  if (!(await isAuthenticated())) redirect("/login");
+  try {
+    await db
+      .insert(post)
+      .values({ title, image, content, categoryId, authorId });
+  } catch (err) {
+    if (err instanceof DrizzleError) throw new Error("Database Error");
+  }
+}
+
+export async function createDraft(
+  title: string,
+  image: string | null,
+  content: string,
+  authorId: string,
+  categoryId: string,
+) {
+  if (!(await isAuthenticated())) redirect("/login");
+  try {
+    await db
+      .insert(draft)
+      .values({ title, image, content, categoryId, authorId });
+  } catch (err) {
+    if (err instanceof DrizzleError) throw new Error("Database Error");
+  }
 }
