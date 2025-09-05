8a954157d55d1f4390a0540ef4bd3f8c80b88953
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-09-06 00:53:39 +0100

    add loading button component

---
+++ components/ui/loading-button.tsx
@@ -0,0 +1,29 @@
+import { Loader2 } from "lucide-react";
+import { Button } from "./button";
+
+interface LoadingButtonProps {
+  variant:
+    | "default"
+    | "secondary"
+    | "outline"
+    | "ghost"
+    | "link"
+    | "destructive";
+  size: "lg" | "sm" | "default" | "icon";
+  className: string;
+}
+
+export default function LoadingButton({
+  size,
+  variant,
+  className,
+}: LoadingButtonProps) {
+  return (
+    <Button variant={variant} size={size} className={className} disabled>
+      <div className="flex gap-2 items-center">
+        <Loader2 size="45" className="animate-spin" />
+        Please wait
+      </div>
+    </Button>
+  );
+}
