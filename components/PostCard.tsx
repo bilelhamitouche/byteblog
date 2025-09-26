c52651ce39ba3a3d4c7e52bdc6248ca0ae88ec30
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-09-26 01:20:32 +0100

    add markup for author avatar and post image

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
 import { Card, CardDescription, CardTitle } from "./ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 interface PostCardProps {
   title: string;
   image: string;
   content: string;
   author: string;
+  authorImage: string;
 }
 
 export default function PostCard({
@@ -12,11 +15,28 @@ export default function PostCard({
   image,
   content,
   author,
+  authorImage,
 }: PostCardProps) {
   return (
-    <Card>
-      <CardDescription>{author}</CardDescription>
-      <CardTitle>{title}</CardTitle>
+    <Card className="flex flex-col gap-2 items-start">
+      <Avatar>
+        <AvatarImage src={author} alt={`${author} image`} />
+        <AvatarFallback>{author[0]}</AvatarFallback>
+      </Avatar>
+      <div className="flex items-center">
+        <CardDescription>{author}</CardDescription>
+        <CardTitle>{title}</CardTitle>
+        {authorImage ? (
+          <Image
+            src={image}
+            alt={`${title} image`}
+            width="48"
+            height="48"
+            className="size-48"
+          />
+        ) : null}
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-700">{content[85]}</p>
     </Card>
   );
 }
