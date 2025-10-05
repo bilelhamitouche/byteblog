940edc67ff77bb76680486eb469b34a03fc3f257
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-05 03:26:35 +0100

    refactor: allow users to create drafts and posts

--- actions/posts.ts
+++ actions/posts.ts
@@ -1,9 +1,42 @@
 "use server";
 
+import { createDraft, createPost } from "@/lib/queries";
+import { writePostSchema } from "@/lib/zod";
+import { getUserInfo } from "./auth";
+
 export async function createPostAction(formData: FormData) {
-  console.log("Post:", formData);
+  const userId = (await getUserInfo())?.id;
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
+  const draftId = formData.get("draftId");
+  const validationResult = writePostSchema.safeParse({ title, image, content })
+  if (!validationResult.success) return {
+    errors: validationResult.error.flatten().fieldErrors,
+  }
+  try {
+    await createPost(validationResult.data.image, validationResult.data.image, validationResult.data.content, userId as string, draftId as string);
+  } catch (err) {
+    return {
+      message: err,
+    }
+  }
 }
 
 export async function createDraftAction(formData: FormData) {
-  console.log("Draft:", formData);
+  const userId = (await getUserInfo())?.id;
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
+  const validationResult = writePostSchema.safeParse({ title, image, content })
+  if (!validationResult.success) return {
+    errors: validationResult.error.flatten().fieldErrors,
+  }
+  try {
+    await createDraft(validationResult.data.title, validationResult.data.image, validationResult.data.content, userId as string);
+  } catch (err) {
+    return {
+      message: err,
+    }
+  }
 }
