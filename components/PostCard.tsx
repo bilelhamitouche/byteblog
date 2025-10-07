01a272a5fa24d9da19077b583db3d5d40af7f129
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-07 16:43:13 +0100

    chore: change some styles and make author name a link

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Card, CardDescription, CardTitle } from "./ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
 interface PostCardProps {
   title: string;
@@ -18,25 +19,28 @@ export default function PostCard({
   authorImage,
 }: PostCardProps) {
   return (
-    <Card className="flex flex-col gap-2 items-start">
-      <Avatar>
-        <AvatarImage src={author} alt={`${author} image`} />
-        <AvatarFallback>{author[0]}</AvatarFallback>
-      </Avatar>
-      <div className="flex items-center">
-        <CardDescription>{author}</CardDescription>
-        <CardTitle>{title}</CardTitle>
+    <Card className="flex flex-col gap-2 items-start p-4 max-w-xl">
+      <Link href="" className="flex items-center gap-2 group">
+        <Avatar>
+          <AvatarImage src={author} alt={`${author} image`} />
+          <AvatarFallback>{author[0]}</AvatarFallback>
+        </Avatar>
+        <CardDescription className="group-hover:underline">{author}</CardDescription>
+      </Link>
+      <div className="flex items-center gap-4">
         {authorImage ? (
           <Image
-            src={image}
+            src={image as string}
             alt={`${title} image`}
-            width="48"
-            height="48"
-            className="size-48"
+            width="120"
+            height="90"
           />
         ) : null}
+        <div className="flex flex-col items-start gap-2">
+          <CardTitle className="text-xl font-bold">{title}</CardTitle>
+          <p className="text-sm text-gray-500 dark:text-gray-300">{content}</p>
+        </div>
       </div>
-      <p className="text-sm text-gray-500 dark:text-gray-700">{content[85]}</p>
     </Card>
   );
 }
