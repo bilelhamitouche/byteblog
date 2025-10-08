533430307ab45f38d830515d93735cdf3f7a7b30
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-08 04:42:36 +0100

    refactor: remove draft actions and change post actions to fit published bool

--- actions/posts.ts
+++ actions/posts.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { createDraft, createPost, updateDraft } from "@/lib/queries";
+import { createPost } from "@/lib/queries";
 import { writePostSchema } from "@/lib/zod";
 import { getUserInfo } from "./auth";
 
@@ -9,46 +9,17 @@ export async function createPostAction(formData: FormData) {
   const title = formData.get("title");
   const image = formData.get("image");
   const content = formData.get("content");
-  const draftId = formData.get("draftId");
+  const published = formData.get("published");
   const validationResult = writePostSchema.safeParse({ title, image, content })
   if (!validationResult.success) return {
     errors: validationResult.error.flatten().fieldErrors,
   }
   try {
-    await createPost(validationResult.data.title, validationResult.data.image, validationResult.data.content, userId as string, draftId as string);
+    console.log("Posting");
+    await createPost(validationResult.data.title, validationResult.data.image, validationResult.data.content, JSON.parse(published as string) as boolean, userId as string);
+    console.log("Posted");
   } catch (err) {
-    return {
-      message: err,
-    }
-  }
-}
-
-export async function createDraftAction(formData: FormData) {
-  const userId = (await getUserInfo())?.id as string;
-  const title = formData.get("title");
-  const image = formData.get("image");
-  const content = formData.get("content");
-  const validationResult = writePostSchema.safeParse({ title, image, content })
-  if (!validationResult.success) return {
-    errors: validationResult.error.flatten().fieldErrors,
-  }
-  try {
-    await createDraft(validationResult.data.title, validationResult.data.image, validationResult.data.content, userId);
-  } catch (err) {
-    return {
-      message: err,
-    }
-  }
-}
-
-export async function editDraftAction(formData: FormData) {
-  const id = formData.get("id") as string;
-  const title = formData.get("title") as string;
-  const image = formData.get("image") as string;
-  const content = formData.get("content") as string;
-  try {
-    await updateDraft(id, title, image, content);
-  } catch (err) {
+    console.log(err)
     return {
       message: err,
     }
