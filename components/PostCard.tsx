2094fe6c66ef22f7bb32489309684148aff0ff19
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-10 02:21:54 +0100

    feat: add post link to post and author page

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -4,23 +4,27 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
 
 interface PostCardProps {
+  id: string;
   title: string;
   image: string | null;
   content: string;
   author: string;
+  authorUsername: string;
   authorImage: string | null;
 }
 
 export default function PostCard({
+  id,
   title,
   image,
   content,
   author,
+  authorUsername,
   authorImage,
 }: PostCardProps) {
   return (
     <Card className="flex flex-col gap-2 items-start p-4 max-w-xl">
-      <Link href="" className="flex items-center gap-2 group">
+      <Link href={`/@${authorUsername}`} className="flex items-center gap-2 group">
         <Avatar>
           <AvatarImage src={author} alt={`${author} image`} />
           <AvatarFallback>{author[0]}</AvatarFallback>
@@ -34,10 +38,13 @@ export default function PostCard({
             alt={`${title} image`}
             width="120"
             height="90"
+            className="rounded-md"
           />
         ) : null}
         <div className="flex flex-col items-start gap-2">
-          <CardTitle className="text-xl font-bold">{title}</CardTitle>
+          <Link href={`/posts/${id}`}>
+            <CardTitle className="text-xl font-bold">{title}</CardTitle>
+          </Link>
           <p className="text-sm text-gray-500 dark:text-gray-300">{content}</p>
         </div>
       </div>
