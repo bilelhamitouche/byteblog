56bf87578803eb451c2c3abdf2f61f72bb489730
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-10 02:23:35 +0100

    feat: display post

---
+++ app/posts/[postId]/page.tsx
@@ -0,0 +1,18 @@
+import { getPost } from "@/lib/queries";
+import { notFound } from "next/navigation";
+
+export default async function Post({ params }: { params: Promise<{ postId: string }> }) {
+  const postId = (await params).postId;
+  const post = await getPost(postId);
+  if (!post) notFound();
+  return (
+    <div className="max-w-3xl h-full py-28 post mx-auto">
+      <div className="p-4 flex flex-col items-start gap-2">
+        <h1 className="text-3xl font-bold">{post.title}</h1>
+        <p>{post.createdAt?.toLocaleDateString()}</p>
+      </div>
+      <hr />
+      <p className="p-4">{post.content}</p>
+    </div>
+  )
+}
