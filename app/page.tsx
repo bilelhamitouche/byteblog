7226e56a63e04a36e5c640fc04c4ba6852b711da
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-10 02:23:19 +0100

    feat: display post list on login

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { getUserInfo } from "@/actions/auth";
 import LandingPage from "@/components/landing-page";
+import Posts from "@/components/Posts";
 
 export default async function Home() {
   const user = await getUserInfo();
   if (!user) return <LandingPage />;
   return (
+    <Posts />
   );
 }
