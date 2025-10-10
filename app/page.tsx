9f86dbf5f283661edc6224a85421e2c0420a87fd
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-10 02:22:51 +0100

    chore: remove unused signout button and form

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
 import { getUserInfo } from "@/actions/auth";
 import LandingPage from "@/components/landing-page";
-import { Button } from "@/components/ui/button";
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
 
 export default async function Home() {
   const user = await getUserInfo();
   if (!user) return <LandingPage />;
   return (
-    <form
-      action={async () => {
-        "use server";
-        auth.api.signOut({
-          headers: await headers(),
-        });
-      }}
-      className="p-8 py-20"
-    >
-      <Button variant="destructive">Sign Out</Button>
-    </form>
   );
 }
