d1a4eb6e6e151cefaacf41beca31bba09da0784d
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-12 05:05:42 +0100

    styles: change padding

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -7,12 +7,12 @@ export default async function Post({ params }: { params: Promise<{ postId: strin
   if (!post) notFound();
   return (
     <div className="max-w-3xl h-full py-28 post mx-auto">
-      <div className="p-4 flex flex-col items-start gap-2">
+      <div className="p-4 flex flex-col items-start gap-2 w-full">
         <h1 className="text-3xl font-bold">{post.title}</h1>
         <p>{post.createdAt?.toLocaleDateString()}</p>
       </div>
       <hr />
-      <p className="p-4">{post.content}</p>
+      <p className="p-2 py-8 leading-7">{post.content}</p>
     </div>
   )
 }
