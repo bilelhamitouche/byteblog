526f8a4539797743314dd10736bf52c5606eeef8
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-12 05:06:36 +0100

    feat: add author name and  avatar

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,4 +1,5 @@
 import { getPost } from "@/lib/queries";
+import { Heart } from "lucide-react";
 import { notFound } from "next/navigation";
 
 export default async function Post({ params }: { params: Promise<{ postId: string }> }) {
@@ -9,6 +10,11 @@ export default async function Post({ params }: { params: Promise<{ postId: strin
     <div className="max-w-3xl h-full py-28 post mx-auto">
       <div className="p-4 flex flex-col items-start gap-2 w-full">
         <h1 className="text-3xl font-bold">{post.title}</h1>
+      <div className="w-full flex gap-4 items-center p-2">
+        <div className="flex items-center gap-1">
+          <span>{post.likesCount}</span>
+          <Heart size="15" />
+        </div>
         <p>{post.createdAt?.toLocaleDateString()}</p>
       </div>
       <hr />
