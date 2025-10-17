db5a79e19f98a4b68d616fc590397fa634ae8d00
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-18 00:19:55 +0100

    styles: change padding and spacing

--- app/authors/[authorId]/page.tsx
+++ app/authors/[authorId]/page.tsx
@@ -5,8 +5,8 @@ export default async function Author({ params }: { params: Promise<{ authorId: s
   const user = await getUserByUsernameOrEmail(authorUsername.split("@")[1]);
   const authorProfile = await getAuthorBio(user.id);
   return (
-    <div className="p-8 py-28 w-full h-full flex">
-      <div className="p-8">
+    <div className="px-8 py-24 w-full h-full flex">
+      <div className="flex items-start space-y-4">
         <h1 className="text-3xl font-bold">{user.name}</h1>
         <p className="text-gray-500 dark:text-gray-300">{authorProfile?.bio}</p>
       </div>
