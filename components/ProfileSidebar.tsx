00dbaa0336f82e5fe01cefbb43a18287907d7b6a
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-18 02:06:21 +0100

    feat: add followers with authorId

--- components/ProfileSidebar.tsx
+++ components/ProfileSidebar.tsx
@@ -1,13 +1,17 @@
+import { getFollowedAuthors } from "@/lib/queries";
 import Image from "next/image";
 import Link from "next/link";
 
 interface ProfileSidebarProps {
+  authorId: string;
   authorName: string;
   authorImage: string | null;
   authorUsername: string;
 }
 
-export default async function ProfileSidebar({ authorName, authorImage, authorUsername }: ProfileSidebarProps) {
+export default async function ProfileSidebar({ authorId, authorName, authorImage, authorUsername }: ProfileSidebarProps) {
+  const followers = await getFollowedAuthors(authorId);
+  console.log(followers);
   return (
     <aside className="border-r py-28 min-w-xs hidden md:flex flex-col items-center gap-2">
       <Image src={authorImage as string} alt={`${authorName} image`} width="100" height="100" className="rounded-full" />
