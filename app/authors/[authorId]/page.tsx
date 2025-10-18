6a0eae1ba86f03c85a804cb0e0cbc3e5716ae632
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-19 00:00:16 +0100

    feat: add skeleton fallback for loading Profile Sidebar

--- app/authors/[authorId]/page.tsx
+++ app/authors/[authorId]/page.tsx
@@ -1,5 +1,7 @@
 import ProfileSidebar from "@/components/ProfileSidebar";
+import ProfileSidebarSkeleton from "@/components/ProfileSidebarSkeleton";
 import { getAuthorBio, getUserByUsernameOrEmail } from "@/lib/queries";
+import { Suspense } from "react";
 
 export default async function Author({ params }: { params: Promise<{ authorId: string }> }) {
   const authorUsername = decodeURIComponent((await params).authorId);
@@ -7,8 +9,10 @@ export default async function Author({ params }: { params: Promise<{ authorId: s
   const authorProfile = await getAuthorBio(user.id);
   return (
     <div className="w-full h-full flex">
-      <ProfileSidebar authorId={user.id} authorName={user.name} authorImage={user.image} authorUsername={authorUsername} />
-      <div className="flex items-start space-y-4 py-20">
+      <Suspense fallback={<ProfileSidebarSkeleton />}>
+        <ProfileSidebar authorId={user.id} authorName={user.name} authorImage={user.image} authorUsername={authorUsername} />
+      </Suspense>
+      <div className="flex items-start space-y-4 py-20 px-8">
         <h1 className="text-3xl font-bold">{user.name}</h1>
         <p className="text-gray-500 dark:text-gray-300">{authorProfile?.bio}</p>
       </div>
