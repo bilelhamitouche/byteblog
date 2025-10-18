9a53fd2bf43f5b6f7a970f40e0ca53aa9452c45f
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-18 02:06:55 +0100

    feat: add authorId to the Profile sidebar

--- app/authors/[authorId]/page.tsx
+++ app/authors/[authorId]/page.tsx
@@ -1,3 +1,4 @@
+import ProfileSidebar from "@/components/ProfileSidebar";
 import { getAuthorBio, getUserByUsernameOrEmail } from "@/lib/queries";
 
 export default async function Author({ params }: { params: Promise<{ authorId: string }> }) {
@@ -5,12 +6,12 @@ export default async function Author({ params }: { params: Promise<{ authorId: s
   const user = await getUserByUsernameOrEmail(authorUsername.split("@")[1]);
   const authorProfile = await getAuthorBio(user.id);
   return (
-    <div className="px-8 py-24 w-full h-full flex">
-      <div className="flex items-start space-y-4">
+    <div className="w-full h-full flex">
+      <ProfileSidebar authorId={user.id} authorName={user.name} authorImage={user.image} authorUsername={authorUsername} />
+      <div className="flex items-start space-y-4 py-20">
         <h1 className="text-3xl font-bold">{user.name}</h1>
         <p className="text-gray-500 dark:text-gray-300">{authorProfile?.bio}</p>
       </div>
-      <aside></aside>
     </div>
   )
 }
