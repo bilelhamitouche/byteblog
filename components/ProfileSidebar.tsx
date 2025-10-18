d7d192710cb97fe41ee4e97eeef0a2e5ee12ed28
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-18 01:41:17 +0100

    feat: add profile sidebar

---
+++ components/ProfileSidebar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProfileSidebarProps {
+  authorName: string;
+  authorImage: string | null;
+  authorUsername: string;
+}
+
+export default async function ProfileSidebar({ authorName, authorImage, authorUsername }: ProfileSidebarProps) {
+  return (
+    <aside className="border-r py-28 min-w-xs hidden md:flex flex-col items-center gap-2">
+      <Image src={authorImage as string} alt={`${authorName} image`} width="100" height="100" className="rounded-full" />
+      <span className="text-lg font-medium">{authorName}</span>
+      <Link href={`/authors/${authorUsername}/edit`} className="text-primary text-base">Edit Profile</Link>
+    </aside>
+  )
+}
