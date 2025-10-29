47641ea6237b08a792627c009a0eb2fb3c829105
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-29 16:41:10 +0100

    chore: cleanup the decoding url code

--- app/authors/[authorId]/page.tsx
+++ app/authors/[authorId]/page.tsx
@@ -3,19 +3,29 @@ import ProfileSidebarSkeleton from "@/components/ProfileSidebarSkeleton";
 import { getAuthorBio, getUserByUsernameOrEmail } from "@/lib/queries";
 import { Suspense } from "react";
 
-export default async function Author({ params }: { params: Promise<{ authorId: string }> }) {
-  const authorUsername = decodeURIComponent((await params).authorId);
+export default async function Author({
+  params,
+}: {
+  params: Promise<{ authorId: string }>;
+}) {
+  const parameters = await params;
+  const authorUsername = decodeURIComponent(parameters.authorId);
   const user = await getUserByUsernameOrEmail(authorUsername.split("@")[1]);
   const authorProfile = await getAuthorBio(user.id);
   return (
-    <div className="w-full h-full flex">
+    <div className="flex w-full h-full">
       <Suspense fallback={<ProfileSidebarSkeleton />}>
-        <ProfileSidebar authorId={user.id} authorName={user.name} authorImage={user.image} authorUsername={authorUsername} />
+        <ProfileSidebar
+          authorId={user.id}
+          authorName={user.name}
+          authorImage={user.image}
+          authorUsername={authorUsername}
+        />
       </Suspense>
-      <div className="flex items-start space-y-4 py-20 px-8">
+      <div className="flex items-start py-20 px-8 space-y-4">
         <h1 className="text-3xl font-bold">{user.name}</h1>
         <p className="text-gray-500 dark:text-gray-300">{authorProfile?.bio}</p>
       </div>
     </div>
-  )
+  );
 }
