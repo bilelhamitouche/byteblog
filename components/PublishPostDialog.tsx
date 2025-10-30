5d7f8ebcb358a578c52ef8e78565b1e41a4612d2
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-10-30 22:38:37 +0100

    feat: add multiple selector component

--- components/PublishPostDialog.tsx
+++ components/PublishPostDialog.tsx
@@ -13,6 +13,7 @@ import {
 import { createPostAction, editPostAction } from "@/actions/posts";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import MultipleSelector from "./ui/multiple-selector";
 
 interface PublishPostDialogProps {
   id?: string;
@@ -58,7 +59,21 @@ export default function PublishPostDialog(postData: PublishPostDialogProps) {
       <DialogContent>
         <DialogTitle>Publish Post</DialogTitle>
         <DialogDescription>Select post topics</DialogDescription>
-        <form>
+        <form className="space-y-4">
+          <MultipleSelector
+            placeholder="Select topics"
+            maxSelected={5}
+            hidePlaceholderWhenSelected
+            defaultOptions={[
+              { value: "javascript", label: "Javascript" },
+              { value: "typescript", label: "Typescript" },
+              { value: "ruby", label: "Ruby" },
+              { value: "python", label: "Python" },
+              { value: "c", label: "C" },
+              { value: "cpp", label: "C++" },
+              { value: "cs", label: "C#" },
+            ]}
+          />
           <DialogFooter className="flex gap-2 items-center">
             <DialogClose asChild>
               <Button onClick={publishPost} disabled={isPublishing}>
