f42c1c63b5f66e28cec80c9199b15f8f2686595d
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-11-14 20:34:48 +0100

    feat: add empty page for search

---
+++ components/NoSearchResults.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import { Newspaper } from "lucide-react";
+import Link from "next/link";
+
+export default function NoSearchResults() {
+  return (
+    <Empty className="p-28 h-full border border-dashed">
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <Newspaper />
+        </EmptyMedia>
+        <EmptyTitle>No Results</EmptyTitle>
+        <EmptyDescription>No search results found.</EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <Button variant="outline" size="sm">
+          <Link href="/">Browse posts</Link>
+        </Button>
+      </EmptyContent>
+    </Empty>
+  );
+}
