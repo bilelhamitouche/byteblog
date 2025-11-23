74b0dc9995434786114cc5c86a348286f22af85b
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-11-23 07:30:54 +0100

    feat: add header and styling with lusitana font

--- app/posts/search/page.tsx
+++ app/posts/search/page.tsx
@@ -1,6 +1,7 @@
 import PostsSkeleton from "@/components/PostsSkeleton";
 import FilterPosts from "@/components/FilterPosts";
 import { Suspense } from "react";
+import { lusitana } from "@/lib/fonts";
 
 export default async function Search({
   searchParams,
@@ -9,7 +10,10 @@ export default async function Search({
 }) {
   const search = (await searchParams).search as string;
   return (
-    <div className="w-full h-full">
+    <div className="p-8 py-20 w-full h-full md:py-24">
+      <h2 className={`text-3xl font-semibold ${lusitana.className}`}>
+        Search Results
+      </h2>
       <Suspense fallback={<PostsSkeleton />}>
         <FilterPosts search={search} />
       </Suspense>
