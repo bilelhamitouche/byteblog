f61352e03b125209edc6e1d5648ae6db768a4b35
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-11-23 09:45:38 +0100

    chore: add infinite scrolling for post filtering

--- components/FilterPosts.tsx
+++ components/FilterPosts.tsx
@@ -1,9 +1,9 @@
 import { searchPosts } from "@/lib/queries";
-import Posts from "./Posts";
 import NoSearchResults from "./NoSearchResults";
+import InfiniteScrolling from "./InfiniteScrolling";
 
 export default async function FilterPosts({ search }: { search: string }) {
   const posts = await searchPosts(search);
-  if (posts && posts.length === 0) return <NoSearchResults />;
-  return <Posts />;
+  if ((posts && posts.length === 0) || !posts) return <NoSearchResults />;
+  return <InfiniteScrolling initialPosts={posts} />;
 }
