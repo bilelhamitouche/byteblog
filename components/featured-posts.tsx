cba21832c5212ad3456a33ea02eff7de5052a256
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-11-23 06:20:29 +0100

    fix: check if authenticated before redirecting to posts

--- components/featured-posts.tsx
+++ components/featured-posts.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { getUserInfo } from "@/actions/auth";
 
-export default function FeaturedPosts() {
+export default async function FeaturedPosts() {
+  const user = await getUserInfo();
   return (
     <section className="flex flex-col gap-4 items-center py-12 px-8 text-center sm:py-16 md:py-20">
       <h2 className="text-3xl font-bold">Featured Posts</h2>
@@ -10,7 +12,7 @@ export default function FeaturedPosts() {
       </p>
       <div className="gap-8 p-8"></div>
       <Button variant="outline" size="lg" asChild>
-        <Link href="/posts">View All Posts</Link>
+        <Link href={user && user.id ? "/" : "/login"}>View All Posts</Link>
       </Button>
     </section>
   );
