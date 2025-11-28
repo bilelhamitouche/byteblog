755935354e79cca153e076578fc50e5f7b497dd2
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-11-28 21:56:16 +0100

    chore: add placeholder

--- components/SearchPosts.tsx
+++ components/SearchPosts.tsx
@@ -24,6 +24,7 @@ export default function SearchPosts({
     <form onSubmit={onSubmit}>
       <ButtonGroup>
         <Input
+          placeholder="Search Posts"
           onChange={(e) => {
             setSearch(e.target.value);
           }}
