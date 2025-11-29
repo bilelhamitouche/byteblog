09e9c159b1c09255148c4ca72c3d85b7ae900f5f
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-11-29 01:58:36 +0100

    fix: extract queryClient from the component

--- components/QueryClientProvider.tsx
+++ components/QueryClientProvider.tsx
@@ -3,12 +3,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
+
 export default function ClientProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       {children}
