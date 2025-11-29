4e9711b0509d88cff135490b71136edcf99e330c
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-11-29 01:13:24 +0100

    feat: add settings page(and tabs component )

---
+++ app/settings/layout.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import SettingsTabs from "./components/SettingsTabs";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="container justify-center py-24 px-8 mx-auto space-y-4 max-w-3xl h-full">
+      <Button variant="link" className="text-primary" asChild>
+        <Link href="/">
+          <ArrowLeft />
+          <span>Back to Home</span>
+        </Link>
+      </Button>
+      <h1 className="text-2xl font-bold">Settings</h1>
+      <SettingsTabs>{children}</SettingsTabs>
+    </div>
+  );
+}
+
+export default Layout;
--- lib/zod.ts
+++ lib/zod.ts
@@ -34,3 +34,26 @@ export const writePostSchema = z
     content: z.string().trim().min(1, { message: "Is required" }),
   })
   .strict();
+
+export const passwordChangeSchema = z.object({
+  password: z
+    .string()
+    .trim()
+    .min(8, { message: "Must be 8 characters long" })
+    .max(20, { message: "Must be at most 20 characters long" }),
+  new_password: z
+    .string()
+    .trim()
+    .min(8, { message: "Must be 8 characters long" })
+    .max(20, { message: "Must be at most 20 characters long" }),
+  confirm_new_password: z
+    .string()
+    .trim()
+    .min(8, { message: "Must be 8 characters long" })
+    .max(20, { message: "Must be at most 20 characters long" }),
+});
+
+export const accountChangeSchema = z.object({
+  name: z.string().trim().min(1, { message: "Name is required" }),
+  email: z.string().trim().email({ message: "Must be a valid Email address" }),
+});
