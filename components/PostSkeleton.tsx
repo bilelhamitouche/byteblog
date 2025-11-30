feb253c7d8a8154726b653dac281e0b88649da60
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-11-30 02:34:51 +0100

    styles: change post skeleton style to match post card

--- components/PostSkeleton.tsx
+++ components/PostSkeleton.tsx
@@ -1,20 +1,19 @@
 import { Skeleton } from "./ui/skeleton";
-import { Card } from "./ui/card";
+import { Card, CardFooter } from "./ui/card";
 
 export default function PostsSkeleton() {
   return (
-    <Card className="flex flex-col p-4">
-      <div className="flex flex-row gap-2 items-center">
+    <Card className="grid grid-cols-1 gap-4 p-4 min-w-full max-w-xl grid-rows-[200px_1fr_auto] min-h-[28rem]">
+      <Skeleton className="w-full h-200px" />
+      <div className="flex gap-2 items-center">
         <Skeleton className="rounded-full size-10" />
         <Skeleton className="h-6 w-30" />
       </div>
-      <div className="flex flex-row gap-4">
-        <Skeleton className="w-40 h-24 rounded-md" />
-        <div className="flex flex-col gap-4 items-start w-full">
-          <Skeleton className="w-40 h-8" />
-          <Skeleton className="w-full h-10" />
-        </div>
-      </div>
+      <Skeleton className="w-full h-24 rounded-md" />
+      <CardFooter className="flex justify-between items-center p-0 w-full">
+        <Skeleton className="w-24 h-8" />
+        <Skeleton className="size-6" />
+      </CardFooter>
     </Card>
   );
 }
