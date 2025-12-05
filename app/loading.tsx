df44901847e91f9bdf00bc202a0a91d91f8131de
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-12-05 01:14:11 +0100

    styles: change the loading spinner

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,9 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center w-full h-full">
-      <Loader2 className="animate-spin" size="40" />
+    <div className="flex flex-col gap-2 justify-center items-center w-full h-full text-gray-500 dark:text-gray-300">
+      <Loader2 className="animate-spin" size="50" />
+      <span className="text-lg">Loading...</span>
     </div>
   );
 }
