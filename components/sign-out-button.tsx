25100271f69df84f6a241138aad3543900fbe0c2
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-12-11 23:11:46 +0100

    chore: refresh and redirect to /

--- components/sign-out-button.tsx
+++ components/sign-out-button.tsx
@@ -9,7 +9,10 @@ export default function SignOutButton() {
   async function signOut() {
     await authClient.signOut({
       fetchOptions: {
-        onSuccess: () => router.refresh(),
+        onSuccess: () => {
+          router.refresh();
+          router.push("/");
+        },
       },
     });
   }
