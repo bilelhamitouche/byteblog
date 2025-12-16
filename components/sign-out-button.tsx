064867df82504e74527df61fa2cb8ab823a6ee7d
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-12-16 13:29:54 +0100

    feat: invalidate session on sign out

--- components/sign-out-button.tsx
+++ components/sign-out-button.tsx
@@ -3,13 +3,18 @@ import { LogOut } from "lucide-react";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import { DropdownMenuItem } from "./ui/dropdown-menu";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function SignOutButton() {
   const router = useRouter();
+  const queryClient = useQueryClient();
   async function signOut() {
     await authClient.signOut({
       fetchOptions: {
         onSuccess: () => {
+          queryClient.invalidateQueries({
+            queryKey: ["session"],
+          });
           router.push("/");
           router.refresh();
         },
