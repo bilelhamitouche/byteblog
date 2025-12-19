771e60c6d0b371db49d08063bee357c204cd93c2
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-12-19 02:15:25 +0100

    styles: add horizontal padding

--- components/ProfileSidebarSkeleton.tsx
+++ components/ProfileSidebarSkeleton.tsx
@@ -2,10 +2,11 @@ import { Skeleton } from "./ui/skeleton";
 
 export default async function ProfileSidebarSkeleton() {
   return (
-    <aside className="hidden flex-col gap-4 items-center py-28 border-r md:flex min-w-xs">
+    <aside className="hidden flex-col gap-4 items-center py-28 px-8 border-r md:flex min-w-xs">
       <Skeleton className="rounded-full size-25" />
       <Skeleton className="h-6 w-30" />
       <Skeleton className="w-20 h-6" />
+      <Skeleton className="w-full h-20" />
     </aside>
   );
 }
