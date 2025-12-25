dbc7fbde91f8ea71e88360900275dd6bf6b70cd9
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-12-25 04:10:05 +0100

    feat: add comments header

--- components/CommentsSection.tsx
+++ components/CommentsSection.tsx
@@ -5,11 +5,14 @@ import CommentForm from "./CommentForm";
 
 export default function CommentsSection() {
   return (
-    <div className="flex flex-col gap-8">
-      <CommentForm />
-      <Suspense fallback={<Loader2 className="animate-spin" size="30" />}>
-        <CommentInfiniteList />
-      </Suspense>
+    <div className="space-y-4">
+      <h3 className="text-lg">Comments</h3>
+      <div className="flex flex-col gap-8">
+        <CommentForm />
+        <Suspense fallback={<Loader2 className="animate-spin" size="30" />}>
+          <CommentInfiniteList />
+        </Suspense>
+      </div>
     </div>
   );
 }
